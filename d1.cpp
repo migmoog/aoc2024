fc16875958da283c9d58ab3332e1156db3f0cfb0
Jeremy Gordon <orangej0312@gmail.com>
2024-12-11 19:58:16 -0500

    Day 1 Part 1

--- d1.cpp
+++ d1.cpp
@@ -1,51 +1,46 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <algorithm>
+#include <stdlib.h>
 
 using namespace std;
 
 #define IS_NUMC(c) ('0' <= c && c <= '9')
 
-void get_mins(int mins[], vector<int> &nums)
+void tokenize(vector<int> &left, vector<int> &right,
+              const bool addToLeft, const int off, const string input)
 {
-    for (int i = 0; i < (int)nums.size(); i++)
+    if (input[off] == '\0')
+        return;
+
+    int bi = 0;
+    char buf[30];
+    int i = off;
+    while (IS_NUMC(input[i]))
     {
-        if (mins[0] < nums[i])
-        {
-            mins[1] = mins[0];
-            mins[0] = nums[i];
-        }
+        buf[bi++] = input[i];
+        i++;
     }
+    buf[bi] = '\0';
+
+    int toknum = atoi(buf);
+    if (addToLeft)
+        left.push_back(toknum);
+    else
+        right.push_back(toknum);
+
+    while (input[i] != '\0' && !IS_NUMC(input[i]))
+        i++;
+
+    tokenize(left, right, !addToLeft, i, input);
 }
 
 void d1(string input)
 {
-    int start = 0, end;
-    vector<int> left_nums;
-    vector<int> right_nums;
-
-    // parsing inputs
-    const int len = input.length();
-    for (int i = 0; i < len; i++)
-    {
-        if (input[i] != '\n')
-            continue;
-        end = i - 1;
-
-        if (!IS_NUMC(input[start]) || !IS_NUMC(input[end]))
-        {
-            cout << "Invalid Entry in list:\nLeft: " << input[start] << ", Right: " << input[end] << endl;
-            return;
-        }
-
-        int l = input[start] - '0';
-        int r = input[end] - '0';
-
-        left_nums.push_back(l);
-        right_nums.push_back(r);
-
-        start = i+1;
-    }
+    vector<int> left_nums{};
+    vector<int> right_nums{};
+    tokenize(left_nums, right_nums, true, 0, input);
 
     if (left_nums.size() != right_nums.size())
     {
@@ -54,16 +49,12 @@ void d1(string input)
     }
 
     // doing the actual work (first is smallest, second is second smallest)
-    int leftMins[2];
-    get_mins(leftMins, left_nums);
-    int rightMins[2];
-    get_mins(rightMins, right_nums);
-
-    int total_distance = 0;
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
-            total_distance += abs(leftMins[i] - rightMins[j]);       
-        }
+    long total_distance = 0;
+    sort(left_nums.begin(), left_nums.end());
+    sort(right_nums.begin(), right_nums.end());
+    for (size_t i = 0; i < left_nums.size(); i++)
+    {
+        total_distance += abs(left_nums[i] - right_nums[i]);
     }
 
     cout << "Day 1 part 1: " << total_distance << endl;
